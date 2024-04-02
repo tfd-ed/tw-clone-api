527d4302d8a113c903e594ec54ce3d1ec0806d16
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-04-03 00:33:58 +0700

    Install All dependence and config dotenv

--- db/db.js
+++ db/db.js
@@ -5,11 +5,11 @@ const uri = process.env.MONGODB_URI
 
 //To-do retry every 10 seconds
 async function dbConnect() {
-    // console.log(uri)
+    console.log(uri)
     mongoose.connection.on('connected', () => console.log('connected'));
     mongoose.connection.on('open', () => console.log('open'));
     await mongoose.connect(uri, {
-        dbName: 'tw-db'
+        dbName: 'sarpheab-db'
     })
 }
 
